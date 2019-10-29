3066d349d2e251742afa14f902965ea55070fa9b
Michael Wagner <wagnerdmike@gmail.com>
2019-10-29 11:38:04 -0700

    Add redux actions and reducers. Start middleware.

---
+++ src/actions/shared.js
@@ -0,0 +1,17 @@
+import { getInitialData } from '../utils/api'
+import { receiveUsers } from './actions/users'
+import { receivePolls } from './actions/polls'
+import { setAuthedUser } from './actions/authedUser'
+
+const AUTHED_ID = 'dan_abramov'
+
+export function handleInitialData () {
+  return (dispatch) => {
+    return getInitialData()
+      .then(({ users, polls }) => {
+        dispatch(receiveUsers(users))
+        dispatch(receivePolls(polls))
+        dispatch(setAuthedUser(AUTHED_ID))
+      })
+  }
+}
